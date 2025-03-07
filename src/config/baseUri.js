5622e08ef23ade76584a8c7e02969d20da0c2652
minhajgrohance <minhaj.grohance@gmail.com>
2025-03-07 17:02:47 +0530

    api changed

--- src/config/baseUri.js
+++ src/config/baseUri.js
@@ -1,6 +1,6 @@
-export const baseUri = 'http://localhost:3000'
+// export const baseUri = 'http://localhost:3000'
 
-// export const baseUri = 'https://api.reveinfotech.com'
+export const baseUri = 'https://api.reveinfotech.com'
 
 // export const baseUri = 'https://api.beateducations.com'
 
